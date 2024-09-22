4876223349b38e4fd526d36b80507b43df4c6ffc
EKirillov <e.kirillov@pressfoto.ru>
2024-09-22 12:49:40 +0500

    testing

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from docx import Document
 
 # Команда /start
 # Команда /start
-def start(update: Update, context: CallbackContext) -> None:
+'''def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Здравствуйте! Отправьте мне файл своей НИР или ВКР, и я исправлю его в соответствии с ГОСТ.')
 
 # Обработка документов
@@ -35,7 +35,7 @@ def main() -> None:
 
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(MessageHandler(
-        filters.document.mime_type("/shared/AI_assistant/noutbuk/output_requirements_table.docx"),
+        filters.document.mime_type("application/vnd.openxmlformats-officedocument.wordprocessingml.document"),
         handle_document))
     dispatcher.add_handler(MessageHandler(filters.document.mime_type("application/msword"), handle_document))
 
@@ -49,3 +49,57 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+
+
+
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('7381756413:AAECp5pgevxJaKQCi3s4gi5cHLZD-JhEKkI')
+
+@bot.message_handler(content_types=['document'])
+def handle_document(message):
+    if message.document.mime_type in ['application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document']:
+        file_info = bot.get_file(message.document.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+
+        with open('received_file.docx', 'wb') as f:
+            f.write(downloaded_file)
+
+        bot.send_document(message.chat.id, open('received_file.docx', 'rb'))
+    else:
+        bot.send_message(message.chat.id, 'Пожалуйста, отправьте файл в формате .doc или .docx.')
+
+bot.polling()
+'''
+
+
+
+
+
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('7381756413:AAECp5pgevxJaKQCi3s4gi5cHLZD-JhEKkI')
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, 'На связи AI ассистент!')
+
+@bot.message_handler(content_types=['document', 'text'])
+def handle_input(message):
+    if message.content_type == 'document':
+        if message.document.mime_type in ['application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document']:
+            file_info = bot.get_file(message.document.file_id)
+            downloaded_file = bot.download_file(file_info.file_path)
+
+            with open('received_file.docx', 'wb') as f:
+                f.write(downloaded_file)
+
+            bot.send_document(message.chat.id, open('received_file.docx', 'rb'))
+        else:
+            bot.send_message(message.chat.id, 'Это не .doc/.docx!')
+    else:
+        bot.send_message(message.chat.id, 'Это не .doc/.docx!')
+
+bot.polling()
